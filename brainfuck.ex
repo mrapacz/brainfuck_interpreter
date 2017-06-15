3e05d7b79c410886d8b505bd6053a8d08da0c5cb
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-06-16 01:21:57 +0200

    Change private functions to public ones

--- brainfuck.ex
+++ brainfuck.ex
@@ -34,12 +34,12 @@ defmodule Brainfuck do
   end
 
   @doc "Moves code pointer to the right"
-  defp move_code_ptr(state) do
+  def move_code_ptr(state) do
     Map.update!(state, :code_ptr, &(&1 + 1))
   end
 
   @doc "If the value of the current data cell has not yet been initialised, it initialises it to zero"
-  defp initialize_data(state) do
+  def initialize_data(state) do
     if is_nil get_in(state, [:data, state.data_ptr])do
       put_in(state, [:data, state.data_ptr], 0)
     else
@@ -50,7 +50,7 @@ defmodule Brainfuck do
   ### brainfuck ops ###
 
   @doc "Performs action corresponding to the current character in the brainfuck code"
-  defp perform_operation(state) do
+  def perform_operation(state) do
     current_character = String.at(state.code, state.code_ptr)
     case current_character do
        "," -> read_char(state)
@@ -65,45 +65,45 @@ defmodule Brainfuck do
   end
 
   @doc "Reads a single character from the input and puts it into the current data cell"
-  defp read_char(state) do
+  def read_char(state) do
     char = IO.gets(1)
     put_in(state, [:data, state.data_ptr], char)
   end
 
   @doc "Prints a character whose ASCII value corresponds to the one in the current data cell"
-  defp print_char(state) do
+  def print_char(state) do
     current_data = get_in(state, [:data, state.data_ptr])
     IO.write <<current_data>>
     state
   end
 
   @doc "Increments the value stored in the current data cell"
-  defp increment_data(state) do
+  def increment_data(state) do
     update_in(state, [:data, state.data_ptr], &(&1 + 1))
   end
 
   @doc "Decrements the value stored in the current data cell"
-  defp decrement_data(state) do
+  def decrement_data(state) do
     update_in(state, [:data, state.data_ptr], &(&1 - 1))
   end
 
   @doc "Moves the data pointer to the right"
-  defp increment_pointer(state) do
+  def increment_pointer(state) do
     Map.update!(state, :data_ptr, &(&1 + 1))
   end
 
   @doc "Moves the data pointer to the left"
-  defp decrement_pointer(state) do
+  def decrement_pointer(state) do
     Map.update!(state, :data_ptr, &(&1 - 1))
   end
 
   @doc "Moves the code pointer to the matching bracket"
-  defp move_to_matching_bracket(state) do
+  def move_to_matching_bracket(state) do
     Map.put(state, :code_ptr, get_in(state, [:brackets, state.code_ptr]))
   end
 
   @doc "Moves to the matching closing bracket if the value in the current data cell equals zero, otherwise does nothing"
-  defp open_bracket(state) do
+  def open_bracket(state) do
     current_data_value = get_in(state, [:data, state.data_ptr])
     if current_data_value == 0 do
       move_to_matching_bracket(state)
@@ -113,7 +113,7 @@ defmodule Brainfuck do
   end
 
   @doc "Moves to the matching opening bracket if the value in the current data cell equals zero, otherwise does nothing"
-  defp close_bracket(state) do
+  def close_bracket(state) do
     current_data_value = get_in(state, [:data, state.data_ptr])
       if current_data_value != 0 do
         move_to_matching_bracket(state)
